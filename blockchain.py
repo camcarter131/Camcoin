6e1de8d014da38442d491eb5a78dfe0e986bf0b1
Cameron Carter <cameroncarter@utexas.edu>
2019-07-07 21:57:28 -0700

    User input for transactions

--- blockchain.py
+++ blockchain.py
@@ -8,8 +8,8 @@ def get_last_blockchain_value():
 def add_value(amount, last_transaction=[1]):
     blockchain.append([last_transaction, amount])
 
-input("Enter amount to add: ")
-add_value(2)
+tx_amount = input("Your transaction amount: ")
+add_value(tx_amount)
 add_value(last_transaction=get_last_blockchain_value(), amount=0.7)
 add_value(4, get_last_blockchain_value())
 print(blockchain)
